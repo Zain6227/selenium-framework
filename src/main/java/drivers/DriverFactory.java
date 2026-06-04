108fdbe78f32c7ec63a11805b2acfe752635563a
Zain6227 <zainulabedinshk123@gmail.com>
2026-06-04 16:06:41 +0530

    Add Jenkins chrome options

--- src/main/java/drivers/DriverFactory.java
+++ src/main/java/drivers/DriverFactory.java
@@ -2,13 +2,25 @@ package drivers;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class DriverFactory {
 
     public static WebDriver driver;
 
     public static WebDriver initDriver() {
-        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+
+options.addArguments("--headless=new");
+options.addArguments("--window-size=1920,1080");
+options.addArguments("--disable-gpu");
+options.addArguments("--disable-extensions");
+options.addArguments("--disable-dev-shm-usage");
+options.addArguments("--remote-allow-origins=*");
+options.addArguments("--disable-background-networking");
+options.addArguments("--disable-features=Translate");
+
+        driver = new ChromeDriver(options);
 
         driver.manage().window().maximize();
 
